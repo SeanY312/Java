30b85473658e3cacd8d7dfedda9a16190afe9e7a
SeanY312 <seany18@nycstudents.net>
2025-09-11 13:22:49 +0000

    .

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Sean York");
+   System.out.println("Sean York 123 Plae Grown Stret");
+    
+  }
+
+  
+}
